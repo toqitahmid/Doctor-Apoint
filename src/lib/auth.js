79c9aa4c430f0a59f23352169624fff3423f1d32
Toqi Tahmid <rubaiathali@gmail.com>
2026-07-08 21:22:07 +0600

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -15,7 +15,8 @@ export const auth = betterAuth({
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret:
+      process.env.GOOGLE_CLIENT_SECRET,
     },
   },
 });
